888f0eb7ea554aa0acb4b38bb1eb83b504995a8e
98WuG <gwu02251250@gmail.com>
2018-06-30 20:20:53 -0400

    update orthodox keymap

--- keyboards/orthodox/keymaps/jerry/keymap.c
+++ keyboards/orthodox/keymaps/jerry/keymap.c
@@ -37,21 +37,23 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
   DVORAK,
+  LOWER,
+  RAISE
 };
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-#define LS__SPC MT(MOD_LSFT, KC_SPC)
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+//#define LS__SPC MT(MOD_LSFT, KC_SPC)
+//#define LOWER MO(_LOWER)
+//#define RAISE MO(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT( \
   	KC_ESC,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
-    CTL_T(KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_LBRC, KC_LCBR,          KC_RCBR, KC_RBRC,          KC_H,    KC_J,    KC_K,    KC_L,    LT(_VIM,KC_SCLN), CTL_T(KC_ENT), \
+    CTL_T(KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_LCBR, KC_LBRC,          KC_RBRC, KC_RCBR,          KC_H,    KC_J,    KC_K,    KC_L,    LT(_VIM,KC_SCLN), CTL_T(KC_ENT), \
     KC_LSPO,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   RAISE,   KC_LGUI, KC_SPC,           KC_LALT, KC_SPC, LOWER,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC \
   ),
 
@@ -70,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT( \
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      											 KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE, \
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_LCTL, _______,          _______, KC_RCTL,          KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC, \
-    _______, KC_UNDS, KC_PLUS, KC_F9,   KC_PIPE, KC_F11,  _______, _______, _______,          _______, _______, _______, KC_F12,  KC_HOME,    KC_COMM,    KC_DOT,  KC_END,  _______ \
+    _______, KC_UNDS, KC_PLUS, KC_TILD, KC_PIPE, KC_F11,  _______, KC_DEL,  _______,           _______, _______, _______, KC_F12,  KC_HOME,    KC_COMM,    KC_DOT,  KC_END,  _______ \
   ),
 
   [_RAISE] = LAYOUT( \
@@ -119,6 +121,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return false;
     break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
   }
   return true;
 }
